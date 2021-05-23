4a4d46ad267076bf7d6bdfc05125e77be8aef86c
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2021-05-23 21:02:13 +0000

    Support multiple clients

--- wsgi.py
+++ wsgi.py
@@ -2,16 +2,28 @@ import sys
 import traceback
 import json
 import os
+from collections import namedtuple
 
 if os.path.dirname(__file__):
   os.chdir(os.path.dirname(__file__))
 
-with open("secrets-server.json") as inf:
-  secrets = json.load(inf)
-  slack_token = secrets["slack"]["token"]
-  simplechat_token = secrets["simplechat"]["token"]
+Client = namedtuple(
+  "Client",
+  ["slack_token",
+   "simplechat_token",
+   "pending_messages"])
 
-pending_messages = []
+clients_by_slack_token = {}
+clients_by_simplechat_token = {}
+for client_name in os.listdir("secrets"):
+  with open(os.path.join("secrets", client_name))as inf:
+    secrets = json.load(inf)
+    client = Client(
+      slack_token=secrets["slack"]["token"],
+      simplechat_token=secrets["simplechat"]["token"],
+      pending_messages=[])
+    clients_by_slack_token[client.slack_token] = client
+    clients_by_simplechat_token[client.simplechat_token] = client
 
 def ok(msg="ok"):
   return "200 OK", [("Content-Type", "text/plain")], msg
@@ -33,19 +45,21 @@ def handle_request(environ, start_response):
   if msgtype == "url_verification":
     return ok(post_json["challenge"])
   elif (msgtype == "event_callback" and
-        post_json["event"]["type"] == "message" and
-        post_json["token"] == slack_token):
-    text = post_json["event"]["text"]
-    userid = post_json["event"]["user"]
-    username = get_name(userid)
-    pending_messages.append("%s: %s" % (username, text))
-    return ok()
-  elif msgtype == "get_messages" and post_json["token"] == simplechat_token:
-    ret = json.dumps(pending_messages)
-    pending_messages.clear()
-    return ok(ret)
-  else:
-    return ok("unknown")
+        post_json["event"]["type"] == "message"):
+    client = clients_by_slack_token.get(post_json["token"], None)
+    if client:
+      text = post_json["event"]["text"]
+      userid = post_json["event"]["user"]
+      username = get_name(userid)
+      client.pending_messages.append("%s: %s" % (username, text))
+      return ok()
+  elif msgtype == "get_messages":
+    client = clients_by_simplechat_token.get(post_json["token"], None)
+    if client:
+      ret = json.dumps(client.pending_messages)
+      client.pending_messages.clear()
+      return ok(ret)
+  return ok("unknown")
 
 def die500(start_response, e):
     trb = traceback.format_exc().encode("utf-8")
